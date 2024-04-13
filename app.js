d22196b097cb7ad2ac8470914e16299e078ce715
Abdulsamad <Samadolalekan15@gmail.com>
2024-04-13 15:20:00 +0100

    creating my portfolio website with Github

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var width = 990;
+
+if (width <= 600){
+  alert('background colour is light blue');
+}  
+   else if( width >= 600 && width <= 900) {
+    alert('background color is light green');
+  } 
+  else if (width > 900){
+    alert('red');
+  }
